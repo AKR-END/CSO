4636f829dfd661cfd3ea60a7bffccd443cd016b2
Ananth Keshav Rajesh <149762262+AKR-END@users.noreply.github.com>
2024-04-15 18:25:12 +0000

    done

--- A2/q1/q1.c
+++ A2/q1/q1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 
-long long max_subarray(long long* arr, long long n, long long l, long long r,long long* deque, long long* pre);
+long long max_subarray(long long* arr, long long n, long long l, long long r,long long* pre, long long* deque);
 
 int main(){
     long long n, l, r;
@@ -16,29 +16,7 @@ int main(){
         scanf("%lld", &arr[i]);
     }
 
-    printf("%lld\n", max_subarray(arr, n, l, r, deque, pre));
+    printf("%lld\n", max_subarray(arr, n, l, r, pre, deque));
     return 0;
 }
 
-
-or(int i=1;i<=n;i++){
-        pre[i]=pre[i-1]+a[i];
-    }
-
-    int deque[n+1];
-    int deque_l=0;
-    int deque_r=0;
-
-    int ans = -1e8;
-
-    for(int i=l;i<=n;i++){
-
-        while(deque_r != deque_l && pre[i-l] < pre[deque[deque_r-1]]) 
-            deque_r--;
-        deque[deque_r] = i-l;
-        deque_r++;
-        if(deque[deque_l] < i-r)
-            deque_l++;
-
-        ans = max(ans,pre[i]-pre[deque[deque_l]]);
-    }
--- A2/q1/q1.s
+++ A2/q1/q1.s
@@ -4,32 +4,78 @@
 # %rdi stores &arr[0], %rsi stores n, %rdx stores l, %rcx stores r, %r8 stores &deque[0], %r9 stores &pre[0]
 
 max_subarray:
-    xorq %r10,%r10  # sum = 0
-    xorq %r11,%r11  # count = 0
-    movq $-100000000,%rbx # max = -100000000
-    movq $0, %r14   # dl = 0
-    movq $0, %r15   # dr = 0
+    
 
-    movq %rdi,%r11  # r11 = &arr[0]
-
-    movq %r9, %r12  # r12 = &pre[0]
-    movq $0, %r13    # r13 = 0
-    movq %r13, %r12 # pre[0] = 0
-    addq $8,%r12    # pre++
+    movq $0,(%r9)   # pre[0] = 0
+    addq $1,%r12    # count = 1
+    movq $0,%r13    # count - 1
 
     jmp .for
 
 .for:
-    cmpq %rsi,%r11 # if count == n
-    je .deque
-    movq (%r11),%r13 # r13 = arr[i]
-    addq %r13,%r10  # sum += arr[i]
-    movq %r10,(%r12) # pre[i] = sum
-    addq $8,%r11    # i++
-    addq $8,%r12    # pre++
-    incq %r11       # count++
+    cmpq %rsi,%r12   # if count == n
+    jg .deque
+
+    movq (%r9,%r13,8),%r14 
+    addq (%rdi,%r13,8),%r14 
+    movq %r14,(%r9,%r12,8) # pre[i] = sum
+
+    addq $1,%r12    # count++
+    addq $1,%r13
     jmp .for
 
 .deque:
-    movq %rdx,%r11  # count = l
+    movq %rdx,%r12  # count = l
+    movq $0, %r13 # count - l
+    movq $0,%r10 # dl = 0
+    movq $0, %r11 # dr = 0
+    movq $-100000000,%rax  # ans = -100000000
+
+.Check:
+    cmpq %rsi,%r12  # if count == n
+    jg .end
+
+.while:
+    cmpq %r10,%r11  # if dl == dr
+    je .loop
+
+    movq -8(%r8, %r11, 8), %r15             
+    movq (%r9, %r15, 8), %r14 
+    movq (%r9,%r13,8),%r15 
+
+    cmpq %r15,%r14 # if pre[count - l] < pre[deque[dr - 1]]
+    jle .loop
+
+    decq %r11 # dr--
+    jmp .while
+
+.loop:
+    movq %r13,(%r8,%r11,8)
+    incq %r11
     
+    cmpq %r10,%r11
+    jle .ans
+
+    movq %r12,%r14
+    subq %rcx,%r14
+    movq (%r8,%r10,8),%r15
+    cmpq %r15,%r14
+    jle .ans
+    
+    incq %r10
+
+.ans:
+    movq (%r9,%r10,8),%r15
+    movq (%r8,%r12,8),%r14
+    subq (%r8,%r15,8),%r14
+
+    cmpq %r14,%rax
+    cmovl %r14,%rax
+
+    incq %r12
+    incq %r13
+    jmp .Check
+
+.end:
+    ret
+
